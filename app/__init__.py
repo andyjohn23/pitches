3d34343fdf9c3fdc842bf41703fb3b5532e217c9
andyjohn23 <andrewnick906@gmail.com>
2020-10-26 01:58:02 +0300

    updated files

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,13 @@ from config import config_options
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
 
+photos = UploadSet('photos',IMAGES)
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 
@@ -17,6 +19,9 @@ def create_app(config_name):
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+
+    # configure UploadSet
+    configure_uploads(app,photos)
 
     #initializing flask extensions
     bootstrap.init_app(app)
