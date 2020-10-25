fe5b7df2b003f7c0fd174dde8e95eb7213ed8e08
andyjohn23 <andrewnick906@gmail.com>
2020-10-25 21:33:53 +0300

    updated files

--- app/auth/views.py
+++ app/auth/views.py
@@ -27,7 +27,7 @@ def login():
 
         flash('Invalid username or Password')
 
-    title = "watchlist login"
+    title = "pitch login"
     return render_template('auth/login.html',login_form = login_form,title=title)
 
 @auth.route('/logout')
