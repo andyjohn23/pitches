d1a503fe21dff8c49d95087e85b4a9431aeaef4c
andyjohn23 <andrewnick906@gmail.com>
2020-10-27 01:25:23 +0300

    updated files

--- manage.py
+++ manage.py
@@ -4,12 +4,13 @@ from app.models import User
 from  flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
-app = create_app('production')
+app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
+
 @manager.command
 def test():
     """Run the unit tests."""
