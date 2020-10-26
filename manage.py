f617b67e4286b3a721e02d1ea1eccc284d573854
andyjohn23 <andrewnick906@gmail.com>
2020-10-26 11:35:07 +0300

    updated files

--- manage.py
+++ manage.py
@@ -6,9 +6,9 @@ from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
 app = create_app('production')
 
+
 manager = Manager(app)
 manager.add_command('server',Server)
-
 migrate = Migrate(app,db)
 manager.add_command('db',MigrateCommand)
 @manager.command
